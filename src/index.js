345e88700f084ef18bb5f5bb3ecbf4826cc61167
RafaelH <rafael0henkel1@gmail.com>
2024-02-02 12:00:14 -0300

    login finished

--- src/index.js
+++ src/index.js
@@ -13,23 +13,22 @@ app.listen(port, () => console.log(`Server started in port: ${port}`));
 // Criar conta
 app.post("/signup", async (req, res) => {
   const data = req.body;
-  const email = data.email;
-  const pass = data.pass;
-  const emailExist = users.find((user) => user.email === email);
+  const { mail, pass } = req.body;
+  const mailExist = users.find((user) => user.mail === mail);
 
-  if (emailExist) {
+  if (mailExist) {
     return res
       .status(400)
       .json({ msg: "este usuario ja esta cadastrado! Tente outro cadastro" });
   }
 
-  const hashPassword = await bcrypt.hash(pass, 10);
-  console.log(hashPassword);
+  const hashPass = await bcrypt.hash(pass, 10);
+
   users.push({
     id: acc,
-    user: data.user,
-    email: data.email,
-    pass: hashPassword,
+    name: data.name,
+    mail: data.mail,
+    pass: hashPass,
   });
 
   acc++;
@@ -37,6 +36,29 @@ app.post("/signup", async (req, res) => {
   res.status(201).json({ msg: "Cadastro efetuado com sucesso!" });
 });
 
+//Login
+app.post("/login", async (req, res) => {
+  const { name, mail, pass } = req.body;
+
+  const user = users.find((user) => user.name === name);
+  const userMail = users.find((user) => user.mail === mail);
+
+  const hashPass = await bcrypt.hash(pass, 10);
+
+  const passMath = await bcrypt.compare(pass, hashPass);
+
+  if (!passMath) {
+    return res.status(400).json({ msg: "Credenciais invalidas" });
+  } else if (!user) {
+    return res.status(400).json({ msg: "Credenciais (nome) invalidas" });
+  } else if (!userMail) {
+    return res.status(400).json({ msg: "Credenciais (email) invalidas" });
+  } else {
+    res.status(200).json({ msg: "Login bem sucedido", data: mail });
+  }
+});
+
+//Mostrar usuarios cadastrados
 app.get("/users", (req, res) => {
   return res.status(200).json({ data: users });
 });
